eb4cbbdc318c5a3277ff44bb3b78e34896991736
csae8092 <peter.andorfer@oeaw.ac.at>
2021-07-21 16:48:49 +0200

    typo

--- train_RTA.sh
+++ train_RTA.sh
@@ -12,6 +12,6 @@ python -m spacy train config.cfg --output ./output --paths.train ./train.spacy -
 echo "eval against default german model"
 python -m spacy evaluate de_core_news_sm ./dev.spacy --output eval_RTA_DE.json
 
-echoe "eval current model"
+echo "eval current model"
 python -m spacy evaluate ./output/model-best ./dev.spacy --output eval_RTA.json
 
